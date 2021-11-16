547be28b813b1648be24776ab14367f7e65b6bb7
complex2liu <contact@complex2math.com>
2021-11-16 16:38:54 +0800

    indentation: exclude NvimTree buffer indentation

--- lua/ui/indentation.lua
+++ lua/ui/indentation.lua
@@ -2,6 +2,9 @@ local M = {}
 
 M.options = {
   char = "|", -- specify the character to be used as indent line
+  bufname_exclude = { 
+    "NvimTree", -- you can see the name of nvim-tree buffer by `:buffers!`
+  }, 
 }
 
 M.setup = function()
