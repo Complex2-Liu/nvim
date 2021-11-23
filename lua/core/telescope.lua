95332828734223b406db2247a17145e4e64d345f
complex2liu <contact@complex2math.com>
2021-11-23 19:57:30 +0800

    telescope: update defaults options
    - Improve ui with `prompt_prefix` and `selection_caret`.
    - Add `--hidden` into `vimgrep_arguments` so that hidden files and
      directories would not be skipped.
    - Customise `defaults.mappings`.
    - Load telescope fzf native sorter to significantly improve sorting
      performance.

--- lua/core/telescope.lua
+++ lua/core/telescope.lua
@@ -5,10 +5,42 @@ M.keymaps = {
   {"n", "<Leader>fg", [[<cmd>lua require('telescope.builtin').live_grep()<cr>]],  { noremap = true, }},
   {"n", "<Leader>fb", [[<cmd>lua require('telescope.builtin').buffers()<cr>]],    { noremap = true, }},
   {"n", "<Leader>fh", [[<cmd>lua require('telescope.builtin').help_tags()<cr>]],  { noremap = true, }},
+M.options = {
+  defaults = {
+    prompt_prefix = "🔭 ", -- the character(s) that will be shown in front of Telescope's prompt
+    selection_caret = " ", -- the character(s) that will shown in front of the current selection
+    sorting_strategy = "ascending", -- so that your cursor is on topmost
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+    },
+
+    -- check out `lua/telescope/mappings.lua` and the `default_mappings` table to see a full list of mappings
+    mappings = {
+      i = {
+        ["<ESC>"] = actions.close, -- not to enter normal-mode in telescope
+        ["<Tab>"] = actions.move_selection_next,
+        ["<S-Tab>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-p>"] = actions.toggle_selection,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+        ["<CR>"] = actions.select_default,
+      },
+    },
+  },
 }
 
 M.setup = function()
+  require("telescope").setup(M.options)
   require("keymaps").set_keymaps(M.keymaps)
+  require("telescope").load_extension("fzf")
 end
 
 return M
